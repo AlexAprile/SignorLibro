88385e1658c8bef329f7f38e065f564f0c3d8005
VincenzoManzoUnisa <v.manzo28@studenti.unisa.it>
2024-02-05 13:33:48 +0100

    Aggiunta di Carrello e Prodotto

---
+++ src/main/java/Storage/Entity/Carrello.java
@@ -0,0 +1,66 @@
+package Storage.Entity;
+
+import java.util.ArrayList;
+
+public class Carrello {
+
+    private ArrayList<ProdottoCarrello> carrello;
+
+
+    public Carrello(){
+        this.carrello = new ArrayList<>();
+    }
+
+    public ArrayList<ProdottoCarrello> getCartItems() {
+        return carrello;
+    }
+
+    public void setCartItems(ArrayList<ProdottoCarrello> cartItems) {
+        this.carrello = cartItems;
+    }
+
+    public void addProduct(Prodotto p, int q){
+        carrello.add(new ProdottoCarrello(p,q));
+    }
+
+    public void removeProduct(Prodotto p){
+
+        for (int i=0; i<carrello.size(); i++){
+
+            if(p.equals(carrello.get(i).getProdotto())){
+                if (carrello.get(i).getQuantita() == 1)
+                    carrello.remove(i);
+                else
+                    carrello.get(i).setQuantita(carrello.get(i).getQuantita()-1);
+            }
+
+        }
+    }
+
+    public boolean isPresent(Prodotto p){
+
+        for(int i=0; i<carrello.size(); i++){
+
+            if( p.equals(carrello.get(i).getProdotto())){
+
+                return true;
+            }
+        }
+        return  false;
+    }
+
+
+
+    public double prezzoTotale(){
+
+        double total=0;
+
+        for (ProdottoCarrello pc: carrello) {
+
+            total+=pc.getProdotto().getPrezzo();
+        }
+        return total;
+    }
+
+
+}
---
+++ src/main/java/Storage/Entity/ProdottoCarrello.java
@@ -0,0 +1,27 @@
+package Storage.Entity;
+
+public class ProdottoCarrello {
+    private Prodotto prodotto;
+    private int quantita;
+
+    public ProdottoCarrello(Prodotto p, int q){
+        this.quantita = q;
+        this.prodotto = p;
+    }
+
+    public Prodotto getProdotto() {
+        return prodotto;
+    }
+
+    public void setProdotto(Prodotto prodotto) {
+        this.prodotto = prodotto;
+    }
+
+    public int getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(int quantita) {
+        this.quantita = quantita;
+    }
+}
