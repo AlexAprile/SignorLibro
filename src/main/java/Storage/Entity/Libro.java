a6a05fcab5f261645e004a5db5ed908e60631314
Giovanni2132 <g.palmigiano2@studenti.unisa.it>
2024-02-05 12:17:49 +0100

    aggiunta variabili in Libro.java

--- src/main/java/Storage/Entity/Libro.java
+++
@@ -1,6 +0,0 @@
-package Storage;
-
-public class Libro {
-    private int isbn;
-    private string titolo;
-}
---
+++ src/main/java/Storage/Entity/Prodotto.java
@@ -0,0 +1,10 @@
+package Storage.Entity;
+
+public class Prodotto {
+    private String titolo;
+    private double prezzo;
+    private String descrizione;
+    private Image copertina;
+    private Date dataPubblicazione;
+    private
+}
