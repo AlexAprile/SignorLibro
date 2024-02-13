9edf0266bca0efda392474cc605c35bc4240c249
Giovanni2132 <g.palmigiano2@studenti.unisa.it>
2024-02-13 14:46:43 +0100

    modifica

--- src/main/java/Application/AutenticazioneController.java
+++ src/main/java/Application/AutenticazioneController.java
@@ -5,7 +5,9 @@ import Storage.DAO.CarrelloDAO;
 import Storage.DAO.UtenteDAO;
 import Storage.Entity.Carrello;
 import Storage.Entity.Utente;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,8 +17,10 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 
+@WebServlet(name = "AurenticazioneController", value = "/AutenticazioneServlet/*")
 public class AutenticazioneController extends HttpServlet {
     String address;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String email = req.getParameter("email");
@@ -24,36 +28,54 @@ public class AutenticazioneController extends HttpServlet {
 
         address = "";
         AutenticaztoneService service=new AutenticaztoneService();
-        try {
-            Utente utente = service.login(email,password);
-            if (utente!=null){   // the user is in the db then s/he is registered and can go on
-                // Salva l'utente nella sessione
-                HttpSession session = req.getSession();
-                session.setAttribute("account", utente);
-                // Redirect alla home page dell'utente
-                if(utente.isAdmin()){
-                    address="dashboard";
-                }
-                else {
-                    Carrello carrello= (Carrello) session.getAttribute("cartSession");
-                    if(carrello!=null&& (!utente.isAdmin()) ){
-                        CarrelloDAO dao = new CarrelloDAO();
-                        dao.addCart(carrello,utente.getMail());
-                        session.removeAttribute("cartSession");
-                    }
-                    address="home";
-                }
-            } else {
 
-                // Redirect alla pagina con un messaggio di errore
-                address ="signin?error=1";  // con redirect, la pagina non può essere in WEB-INF, il browser non potrebbe raggiungerla
-            }
-            resp.sendRedirect(address);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+        String  path=(req.getPathInfo()!=null) ? req.getPathInfo():"/";
+        RequestDispatcher dispatcher;
+
+        switch (path){
+            case "/login":
+                try {
+                    Utente utente = service.login(email,password);
+                    if (utente!=null){   // the user is in the db then s/he is registered and can go on
+                        // Salva l'utente nella sessione
+                        HttpSession session = req.getSession();
+                        session.setAttribute("account", utente);
+                        // Redirect alla home page dell'utente
+                        if(utente.isAdmin()){
+                            address="dashboard";
+                        }
+                        else {
+                            Carrello carrello= (Carrello) session.getAttribute("cartSession");
+                            if(carrello!=null&& (!utente.isAdmin()) ){
+                                CarrelloDAO dao = new CarrelloDAO();
+                                dao.addCart(carrello,utente.getMail());
+                                session.removeAttribute("cartSession");
+                            }
+                            address="home";
+                        }
+                    } else {
+
+                        // Redirect alla pagina con un messaggio di errore
+                        address ="signin?error=1";  // con redirect, la pagina non può essere in WEB-INF, il browser non potrebbe raggiungerla
+                    }
+                    resp.sendRedirect(address);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                } catch (NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                }
+                resp.sendRedirect(address);
+
+                dispatcher =req.getRequestDispatcher("/WEB-INF/views/client/login1.jsp");
+                dispatcher.forward(req,resp);
+                break;
+            case "/logout":
+
+                service.logout(req.getSession());
+                resp.sendRedirect("/WEB-INF/index.jsp");
+                break;
         }
-        resp.sendRedirect(address);
+
+
     }
 }
