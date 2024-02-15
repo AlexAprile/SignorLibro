5a48877f959fe17b353eedca56b74c8b28152f8f
Alessandro Aprile <a,aprile8@studenti.unisa.it>
2024-02-15 16:08:21 +0100

    RegistrazioneTest modifica e aggiunta test

--- src/main/java/RegistrazioneTest.java
+++ src/main/java/RegistrazioneTest.java
@@ -1,26 +1,12 @@
-import Application.AutenticazioneController;
-
-        import Storage.RegistrazioneService;
-        import http.AccountValidator;
-        import http.AccountValidatorUpdate;
-        import http.InvalidRequestException;
-        import http.RequestValidator;
-        import jakarta.servlet.*;
-        import jakarta.servlet.http.*;
-        import org.junit.Assert;
-        import org.junit.Before;
-        import org.junit.Test;
-        import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-
-import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.UnsupportedEncodingException;
-        import java.security.NoSuchAlgorithmException;
-        import java.security.Principal;
-        import java.util.*;
-
-        import static org.mockito.Mockito.mock;
+import http.AccountValidatorUpdate;
+import http.RequestValidator;
+import jakarta.servlet.http.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import static org.mockito.Mockito.mock;
 
 public class RegistrazioneTest {
 
@@ -41,7 +27,7 @@ public class RegistrazioneTest {
             ArrayList<String> lista=(ArrayList<String>) validator.getErrors();
             for (String temp:lista)
                 System.out.println(temp);
-            Assert.assertEquals(false,validator.hashErrors());
+            Assert.assertEquals(true,validator.hashErrors());
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
@@ -56,7 +42,7 @@ public class RegistrazioneTest {
             ArrayList<String> lista=(ArrayList<String>) validator.getErrors();
             for (String temp:lista)
                 System.out.println(temp);
-            Assert.assertEquals(false,validator.hashErrors());
+            Assert.assertEquals(true,validator.hashErrors());
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
@@ -68,6 +54,21 @@ public class RegistrazioneTest {
             RequestValidator validator=AccountValidatorUpdate.validateUpForm(request,
                     "a.aprile8@studenti.unisa.it",
                     "Alessandro1","Aprile","Aprile08");
+            ArrayList<String> lista=(ArrayList<String>) validator.getErrors();
+            for (String temp:lista)
+                System.out.println(temp);
+            Assert.assertEquals(true,validator.hashErrors());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void TestRegistrazione2_8(){
+        try {
+            RequestValidator validator = AccountValidatorUpdate.validateUpForm(request,
+                    "a.aprile8@studenti.unisa.it",
+                    "Alessandro","Aprile","Aprile08");
             ArrayList<String> lista=(ArrayList<String>) validator.getErrors();
             for (String temp:lista)
                 System.out.println(temp);
