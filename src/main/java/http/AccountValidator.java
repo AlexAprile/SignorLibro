4196d14c5d1ca88800d5e5f338eb35be69d4b882
Alessandro Aprile <a,aprile8@studenti.unisa.it>
2024-02-19 16:50:02 +0100

    regex password

--- src/main/java/http/AccountValidator.java
+++ src/main/java/http/AccountValidator.java
@@ -10,7 +10,7 @@ public class AccountValidator{
             throws NoSuchAlgorithmException {
         RequestValidator validator=new RequestValidator(req);
         validator.assertEmail(email,"Email non valida es. example123@example.com");
-        validator.assertMatch(password,Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"),
+        validator.assertMatch(password,Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$"),
                 "Password non valida es. Abc12345");
         return validator;
     }
--- src/main/java/http/AccountValidatorUpdate.java
+++ src/main/java/http/AccountValidatorUpdate.java
@@ -10,7 +10,7 @@ public class AccountValidatorUpdate {
     public static RequestValidator validateUpForm(HttpServletRequest req, String email, String nome, String cognome, String password, Date nascita) throws NoSuchAlgorithmException {
         RequestValidator validator=new RequestValidator(req);
         validator.assertEmail(email,"Email non valida es. example123@example.com");
-        validator.assertMatch(password,Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"),"la Password non è valida");
+        validator.assertMatch(password,Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$"),"la Password non è valida");
         validator.assertMatch(nome,Pattern.compile("^[A-Za-z ]{3,30}$"),"il nome deve contenere almeno 3 caratteri e al massimo 30");
         validator.assertMatch(cognome,Pattern.compile("^[A-Za-z ]{3,40}$"),"il cognome deve contere almeno 3 caratteri e al massimo 30");
         validator.assertDateMaggiorenne(nascita,"la data non rispetta il formato");
